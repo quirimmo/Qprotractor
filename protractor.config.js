1a1f4d8e4e0af23b065d509824bac8c1de686634
quirimmo <quirimmo@gmail.com>
2017-08-18 17:02:45 +0100

    adding capabilities to protractor.config.js

--- protractor.config.js
+++ protractor.config.js
@@ -5,13 +5,16 @@ fs.readdirSync(SELENIUM_FOLDER).forEach(file => {
     res = file.match(/selenium-server-standalone-(\d{1}.\d{1}.\d{1}).jar/i);
     if (res) {
         seleniumVersion = res[1];
-    } 
+    }
 })
 if (!seleniumVersion) {
     throw new Error('No selenium server jar found inside your protractor node_modules subfolder');
 }
 
 exports.config = {
+    capabilities: {
+        'browserName': 'chrome'
+    },
     seleniumServerJar: `./node_modules/protractor/node_modules/webdriver-manager/selenium/selenium-server-standalone-${seleniumVersion}.jar`,
     specs: [
         './test/base-protractor.spec.js',
