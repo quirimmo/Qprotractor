696f39759c2b2642922e0f83c9b517229eb92973
Tarun Lalwani <tarunlalwani@users.noreply.github.com>
2017-08-18 18:23:04 +0530

    Remove jar

--- protractor.config.js
+++ protractor.config.js
@@ -1,5 +1,4 @@
 exports.config = {
-    seleniumServerJar: './node_modules/protractor/node_modules/webdriver-manager/selenium/selenium-server-standalone-3.5.0.jar',
     specs: [
         './test/base-protractor.spec.js',
         './test/element-finder.spec.js',
